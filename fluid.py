39336334b055a508d416d823fe8cd1a24ea42940
Yi Wang <yi.wang.2005@gmail.com>
2019-12-24 21:14:37 -0800

    Dry run to get step definitions

--- fluid.py
+++ fluid.py
@@ -29,20 +29,18 @@ def task(func):
     return print_taskrun
 
 
-STEPS = []  # For holding steps of a Task.
-
-
 def step(image, cmd, args):
-    '''Append a step to global variable STEPS'''
+    '''Define a step'''
 
     def step_name():
         '''step_name is only supposed to be called by step'''
         # See https://stackoverflow.com/a/6628348/724872.
+        # frame 0 - step_name()
+        # frame 1 - add_step()
+        # frame 2 - fluid.step()
         caller_of_step = inspect.stack()[2]
         filename = caller_of_step[1]
         lineno = caller_of_step[2]
         return k8s.safe_name(f"{filename}-{lineno}")
 
-    global STEPS
-    STEPS.append(
-        tekton.step(step_name(), image, cmd, args))
+    tekton.add_step(step_name(), image, cmd, args)
